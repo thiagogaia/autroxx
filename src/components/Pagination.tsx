a036c938bc4a21e7725634708bc82ed6b16a5041
Thiago Gaia <thiagogaia@gmail.com>
2025-09-22 16:25:27 -0300

    paginacao maior

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -95,6 +95,10 @@ export function Pagination({ className }: PaginationProps) {
               <SelectItem value="10">10</SelectItem>
               <SelectItem value="20">20</SelectItem>
               <SelectItem value="50">50</SelectItem>
+              <SelectItem value="100">100</SelectItem>
+              <SelectItem value="200">200</SelectItem>
+              <SelectItem value="500">500</SelectItem>
+              <SelectItem value="1000">1000</SelectItem>
             </SelectContent>
           </Select>
         </div>
